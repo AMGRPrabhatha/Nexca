426341f7c7573235299b7b3dc64f997cf2a789ee
Rasith ABeywickrama <rasithabeywickrama@Rasiths-MacBook-Air.local>
2026-09-14 19:58:35 +0530

    Temporarily comment out Gallery page links and route

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Home from './pages/Home';
 import Shop from './pages/Shop';
 import About from './pages/About';
 import Blog from './pages/Blog';
-import Gallery from './pages/Gallery';
+// import Gallery from './pages/Gallery';
 import PrivacyPolicy from './pages/PrivacyPolicy';
 import WhatsAppButton from './components/WhatsAppButton';
 
@@ -40,7 +40,7 @@ function App() {
             <Route path="/shop" element={<Shop />} />
             <Route path="/about" element={<About />} />
             <Route path="/blog" element={<Blog />} />
-            <Route path="/gallery" element={<Gallery />} />
+            {/* <Route path="/gallery" element={<Gallery />} /> */}
             <Route path="/privacy-policy" element={<PrivacyPolicy />} />
           </Routes>
         </main>
