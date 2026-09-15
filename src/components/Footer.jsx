21d67f7b82b38e1b647c949cb940df4560e1a974
Rasith ABeywickrama <rasithabeywickrama@Rasiths-MacBook-Air.local>
2026-09-15 12:28:51 +0530

    Remove +81 80 9381 8491 from footer contact info

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -35,7 +35,6 @@ export const Footer = () => {
           <div className="footer-col contact-col">
             <h5>CONTACT INFO</h5>
             <ul>
-              <li><span>+81 80 9381 8491</span></li>
               <li><span>+81 80-5166-2345</span></li>
               <li><span>nexcainfo@gmail.com</span></li>
             </ul>
